510883e9002eab22aad36fc6998f4b25f674832d
k971119 <hanyul97@naver.com>
2025-01-30 16:10:27 +0900

    Initial commit

---
+++ src/test/java/com/inven/gameinven/GameInvenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inven.gameinven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameInvenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
